beaa396f42a2094f490f85509a6cd1da553925cc
Zion <zion2027@gmail.com>
2023-11-27 10:18:08 -0800

    Actions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
+#include <limits>
 #include "Users.h"
 #include "Library.h"
 #include "CD.h"
+
+using namespace std;
 
 void createUsers(Library& library) {
     std::string username, userID;
@@ -26,7 +29,37 @@ bool continueCreatingAccounts(Library& library, std::string option) {
     return true;
 }
 
+int actions() {
+    int action_number;
+    bool validInput = false;
+
+    while (!validInput) {
+        cout << "1. Do you want to update the existing account?" << endl;
+        cout << "2. Do you want to create an account?" << endl;
+        cout << "3. Do you want to borrow a book?" << endl;
+        cout << "4. Do you want to return a book?" << endl;
+        cout << "5. Do you want to see borrowing and returning history of your account?" << endl;
+        cout << "Enter action number: ";
+        cin >> action_number;
+
+        if (action_number >= 1 && action_number <= 5) {
+            validInput = true;
+        } else {
+            cout << "Please enter a number between 1 and 5." << endl;
+            // Clear the input buffer to prevent an infinite loop in case of non-integer input
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+    }
+
+    return action_number;
+}
+
 int main() {
+
+    // get user to enter action they want to perform
+    int act = actions();
+    cout << "Action choosen is: " <<act << endl;
 
     //  class tester
     Users u("asdas", "123");
